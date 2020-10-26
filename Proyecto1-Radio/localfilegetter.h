76cb4d38893c79dffd58561361196d2f7b5bd2ab
Luis Zuniga <luisandreyzuniga@gmail.com>
2020-10-26 10:15:02 -0600

    Adding a method to get all the directories/files in a path

--- Proyecto1-Radio/localfilegetter.h
+++ Proyecto1-Radio/localfilegetter.h
@@ -3,6 +3,8 @@
 #define LOCALFILEGETTER_H
 #include<SongGetter.h>
 #include <filesystem>
+#include<DoubleList/DoubleList.hpp>
+#include<exception>
 namespace fs =std::filesystem ;
 class LocalfileGetter:SongGetter
 {
@@ -13,12 +15,28 @@ public:
     void setSource(QString source);
     void setSouce(std::string source);
     QString getSource();
+    DoubleList<std::string>* getDirectoryList();
+    DoubleList<std::string>* getFilesList();
+    Song getNextFile();
+    std::string getNextDir();
+    void restartIterator();
 private:
     QString* Source_Route;
     std::string getRoute(std::string route, std::string value);
+    DoubleList<std::string>* getRoutesList(bool ISDIRECTORY);
+    fs::directory_iterator myIterator;
+    std::filesystem::directory_entry getNextBooleanCondition(bool dirCondition);
+
 
 
 
 };
+
+struct NotFoundException:public std::exception{
+    const char * what () const throw () {
+        return "The file requested has been not Found";
+    }
+};
+
 
 #endif // LOCALFILEGETTER_H
