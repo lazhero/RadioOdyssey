2dbb58cf68e0a0340bc39bed10ec9c40d4531fbf
adrian <adriantec2019@gmail.com>
2020-10-28 11:33:19 -0600

    autodocumentación dummy

--- Proyecto1-Radio/pages.cpp
+++ Proyecto1-Radio/pages.cpp
@@ -1,18 +1,37 @@
 #include "pages.h"
 int minLen;
+/**
+ * @brief Pages::Pages
+ * @param len
+ */
 Pages::Pages(int len)
 {
     setListLen(len);
 }
+/**
+ * @brief Pages::getListLen
+ * @return
+ */
 int Pages::getListLen(){
     return ListLen;
 }
+
+/**
+ * @brief Pages::setListLen
+ * @param len
+ */
 void Pages::setListLen(int len){
 
     if(lenVerification((len)))ListLen=len;
     else ListLen=minLen;
 }
 
+
+/**
+ * returns if the pages are full
+ * @brief Pages::isFull
+ * @return
+ */
 bool Pages::isFull()
 {
     bool condition=Prev->getLen()==ListLen;
@@ -20,6 +39,13 @@ bool Pages::isFull()
     condition&=Actual->getLen()==ListLen;
     return condition;
 }
+
+
+/**
+ * add a song to prev page
+ * @brief Pages::AddToBack
+ * @param song
+ */
 void Pages::AddToBack(Song song){
     Prev->add(*new Song(song));
     iterateFront();
@@ -27,11 +53,23 @@ void Pages::AddToBack(Song song){
 
 
 }
+/**
+ * add a song to next page
+ * @brief Pages::AddToBack
+ * @param song
+ * @author Luis andrey
+ */
 void Pages::AddToFront(Song song){
     Next->add(*new Song(song));
     iterateBack();
     if(this->Prev->getLen()>ListLen)Prev->erase(minIndex);
 }
+/**
+ * anexates the fist element of the next viewlist to the actual list and the last first element of the current list
+ * with the previus one
+ * @brief Pages::iterateBack
+ * @author Luis andrey
+ */
 void Pages::iterateBack(){
     if(Next->getLen()>minLen){
         Actual->add(*Next->get(minIndex));
@@ -42,6 +80,12 @@ void Pages::iterateBack(){
         Actual->erase(minIndex);
     }
 }
+/**
+ * anexates the fist element of the next viewlist to the actual list and the last first element of the current list
+ * with the previus one
+ * @brief Pages::iterateBack
+ * @author Luis andrey
+ */
 void Pages::iterateFront(){
     if(Prev->getLen()>minLen){
         Actual->add(*Prev->get(Prev->getLen()-1));
@@ -52,6 +96,14 @@ void Pages::iterateFront(){
         Actual->erase(Actual->getLen()-1);
     }
 }
+/**
+ *Verifies that data is natural and positive  number
+ *  @brief Pages::lenVerification
+ * @param data
+ * @return
+ * @author Luis Andrey Zuniga
+ */
+
 bool Pages::lenVerification(int data){
-    return true;
+    return data>=0;
 }
